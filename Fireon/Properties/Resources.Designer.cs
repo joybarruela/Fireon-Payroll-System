962764ca8974c82941cf86b7811c4680efbc274b
joybarruela <barruejoy@gmail.com>
2020-11-24 16:59:15 -0800

    Deleted Unecessary Image Resources

--- Fireon/Properties/Resources.Designer.cs
+++ Fireon/Properties/Resources.Designer.cs
@@ -233,16 +233,6 @@ namespace Fireon.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap btn_close_form {
-            get {
-                object obj = ResourceManager.GetObject("btn_close_form", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap btn_continue {
             get {
                 object obj = ResourceManager.GetObject("btn_continue", resourceCulture);
