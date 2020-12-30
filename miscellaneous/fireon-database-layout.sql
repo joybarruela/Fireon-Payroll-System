2ab68ca2b3102a40e08dacc26b3e1baa15de7daf
51 <21vibiesca@gmail.com>
2020-12-30 23:47:53 +0800

    SQL error fix

--- miscellaneous/fireon-database-layout.sql
+++ miscellaneous/fireon-database-layout.sql
@@ -55,7 +55,7 @@ CREATE TABLE `tbl_allowance` (
   `allowanceName` varchar(45) DEFAULT NULL,
   `allowanceAmount` int DEFAULT '0',
   PRIMARY KEY (`allowanceID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,6 +64,7 @@ CREATE TABLE `tbl_allowance` (
 
 LOCK TABLES `tbl_allowance` WRITE;
 /*!40000 ALTER TABLE `tbl_allowance` DISABLE KEYS */;
+INSERT INTO `tbl_allowance` VALUES (7,'Food Allowance',500);
 /*!40000 ALTER TABLE `tbl_allowance` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -77,9 +78,9 @@ DROP TABLE IF EXISTS `tbl_deduction`;
 CREATE TABLE `tbl_deduction` (
   `deductionID` int NOT NULL AUTO_INCREMENT,
   `deductionName` varchar(45) DEFAULT NULL,
-  `deductionAmount` float DEFAULT NULL,
+  `deductionPercentage` float DEFAULT NULL,
   PRIMARY KEY (`deductionID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,6 +89,7 @@ CREATE TABLE `tbl_deduction` (
 
 LOCK TABLES `tbl_deduction` WRITE;
 /*!40000 ALTER TABLE `tbl_deduction` DISABLE KEYS */;
+INSERT INTO `tbl_deduction` VALUES (3,'SSS',3.63),(4,'PhilHealth',2.75),(5,'PagIBIG',2);
 /*!40000 ALTER TABLE `tbl_deduction` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,7 +122,7 @@ CREATE TABLE `tbl_employee` (
   `employeeImageLocation` varchar(255) DEFAULT NULL,
   `employeeDateEmployed` date DEFAULT NULL,
   PRIMARY KEY (`employeeID`)
-) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=91 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -129,6 +131,7 @@ CREATE TABLE `tbl_employee` (
 
 LOCK TABLES `tbl_employee` WRITE;
 /*!40000 ALTER TABLE `tbl_employee` DISABLE KEYS */;
+INSERT INTO `tbl_employee` VALUES (76,'SERGE','E','VIBIESCA','Male',9326154110,'21VIBIESCA@GMAIL.COM','ADDRESS','2000-08-21','Single','FILIPINO','Customer Service','Customer Care Operator','Regular',216,100,0,'Bill of Exchange',NULL,'2020-12-27'),(77,'MARIE JOY','B','BARRUELA','Female',9195207555,'MJOYBARRUELA@GMAIL.COM','ADDRESS','2000-08-31','Single','FILIPINO','Customer Service','Customer Care Operator','Regular',216,200,0,'Cash Payment',NULL,'2020-12-27'),(78,'CYRUS JEURELL','Y','VILLANUEVA','Male',9131274161,'SAISASU@GMAIL.COM','ADDRESS','2000-10-01','Married','FILIPINO','Administrative','Administrative Assistant Director','Regular',216,300,0,'Bill of Exchange',NULL,'2020-12-27'),(79,'MARY ROSE','B','BRIGOLI','Female',9137127412,'BRIGS@GMAIL.COM','ADDRESS','2000-07-07','Married','FILIPINO','Customer Service','Customer Care Operator','Regular',216,222,0,'Cash Payment',NULL,'2020-12-27'),(80,'SONNY','S','MOORE','Male',9421894842,'SONNY@GMAIL.COM','ADDRESS','1990-06-14','Single','AMERICAN','Administrative','Administrative Coordinator','Regular',216,150,0,'Cheque','D:\\Fireon-Payroll-System\\Fireon\\Resources\\miscellaneous\\miscEmployeeDP.png','2020-12-28'),(81,'NICOLE MAE','','GARCIA','Female',9123178213,'NICOLE@GMAIL.COM','ADDRESS','1999-12-25','Married','FILIPINO','Customer Service','Customer Experience Specialist','Contractual',213,200,3,'Bill of Exchange',NULL,'2020-12-29'),(82,'FELIX','','KJELLBERG','Male',9123312313,'PEWDIEPIE@GMAIL.COM','SWEDEN','1990-06-12','Married','SWEDISH','Marketing','Marketing Director','Regular',216,333,0,'Cheque',NULL,'2020-12-30'),(83,'MICHAEL','','JACKSON','Male',9321312223,'JACKSON@GMAIL.COM','ADDRESS','1800-01-01','Married','AMERICAN','Customer Service','Retail Associate','Regular',216,222,0,'Bill of Exchange',NULL,'2020-12-30'),(84,'MAINE','G','MENDOZA','Female',9321321313,'MAINE@GMAIL.COM','ADDRESS','1990-07-19','Single','FILIPINO','Human Resource','Human Resource Associate','Contractual',100,333,333,'Bank Draft',NULL,'2020-12-30'),(85,'PEDRO','','MARDIGAL','Male',9837621833,'PMADRIGAL@GMAIL.COM','ADDRESS','2020-12-10','Married','FILIPINO','Customer Service','Customer Care Operator','Regular',216,222,0,'Cash Payment',NULL,'2020-12-30'),(86,'ALDEN','F','RICHARDS','Male',9321313131,'AR@GMAIL.COM','ADDRESS','2000-02-01','Married','FILIPINO','Administrative','Administrative Assistant Director','Contractual',333,333,333,'Bank Draft',NULL,'2020-12-30'),(87,'ASD','ASD','ASD','Male',9312312321,'ASD','ADDRESS','2020-12-01','Married','FILIPINO','Administrative','Administrative Coordinator','Contractual',333,333,111,'Bank Draft','C:\\Users\\51\\Desktop\\paint 3d\\12345678.png','2020-12-30'),(88,'E','QWE','QWE','Male',9231313133,'QWE','QWE','2020-12-08','Separated','QWE','Customer Service','Call Center Customer Support','Contractual',123,123,123,'Bill of Exchange',NULL,'2020-12-30'),(89,'SD','ASD','ASD','Male',23133213123,'QWE','QWE','2020-12-02','Widowed','EQE','Customer Service','Customer Care Operator','Regular',216,213,0,'Cheque',NULL,'2020-12-30'),(90,'ASD','DA','ASD','Male',12331231321,'12312213','32133213','2020-12-10','Separated','EWQEQEW','Customer Service','Customer Care Operator','Regular',216,123,0,'Bill of Exchange',NULL,'2020-12-30');
 /*!40000 ALTER TABLE `tbl_employee` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -149,11 +152,15 @@ CREATE TABLE `tbl_employee_details` (
   `holidayHolidayPay` int DEFAULT '0',
   `violationViolationAmount` int DEFAULT '0',
   `cashAdvanceAmount` int DEFAULT '0',
-  `payrollTotalPay` int DEFAULT '0',
+  `payrollTotalWorkingHours` int DEFAULT '0',
+  `payrollBasicSalary` int DEFAULT '0',
+  `payrollTotalDeduction` int DEFAULT '0',
+  `payrollTotalAddition` int DEFAULT '0',
+  `payrollNetPay` int DEFAULT '0',
   PRIMARY KEY (`employeeDetailsID`),
   KEY `fkEmployeeID_idx` (`idtbl_employee_details`),
   CONSTRAINT `fkEmployeeID` FOREIGN KEY (`idtbl_employee_details`) REFERENCES `tbl_employee` (`employeeID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -162,6 +169,7 @@ CREATE TABLE `tbl_employee_details` (
 
 LOCK TABLES `tbl_employee_details` WRITE;
 /*!40000 ALTER TABLE `tbl_employee_details` DISABLE KEYS */;
+INSERT INTO `tbl_employee_details` VALUES (76,12,30,60,180,0,0,0,5000,216,21600,1810,500,20290),(77,13,30,60,180,0,0,0,5000,216,43200,3620,500,40080),(78,14,30,60,180,0,0,0,5000,216,64800,5430,500,59870),(79,15,30,60,180,10,0,0,5000,226,50172,4204,500,46468),(80,16,30,60,180,0,0,0,5000,216,32400,2715,500,30185),(81,17,30,60,180,0,0,0,5000,213,42600,3569,500,39531),(82,18,30,60,180,0,0,0,5000,216,71928,6027,500,66401),(83,19,30,60,180,0,0,0,5000,216,47952,4018,500,44434),(84,20,30,60,180,0,0,0,5000,100,33300,2790,500,31010),(85,21,30,60,180,0,0,0,5000,216,47952,4018,500,44434),(86,22,30,60,180,0,0,0,5000,333,110889,9292,500,102097),(87,23,30,60,180,0,0,0,5000,333,110889,9292,500,102097),(88,24,30,60,180,0,0,0,5000,123,15129,1267,500,14362),(89,25,30,60,180,0,0,0,5000,216,46008,3855,500,42653),(90,26,30,60,180,0,0,0,5000,216,26568,2226,500,24842);
 /*!40000 ALTER TABLE `tbl_employee_details` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -182,4 +190,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-27  0:30:01
+-- Dump completed on 2020-12-30 23:46:16
